85a5990dae989a4886af2fbff763d032e1e48c53
daniel_cohen <daniel1cohen13579@gmail.com>
2022-10-31 23:03:22 +0200

    Updet files

---
+++ Stage0/Program1749.cs
@@ -0,0 +1,22 @@
+﻿namespace Stage0
+{
+    partial class Program9255
+    {
+        static void Main(string[] args)
+        {
+            welcome1749();
+            welcome9255();
+            Console.ReadKey();
+
+
+        }
+
+        static partial void welcome9255();
+        private static void welcome1749()
+        {
+            Console.WriteLine("Enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("{0}, wlcome to my first console application", userName);
+        }
+    }
+}
