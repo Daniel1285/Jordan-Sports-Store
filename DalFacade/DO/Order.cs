2e4615931fa7b33c5ea22e47c33104ab94d549c1
נתן שטרן <natstern@g.jct.ac.il>
2022-11-05 22:42:02 +0200

    Update Do -> Order

--- DalFacade/DO/Order.cs
+++ DalFacade/DO/Order.cs
@@ -1,7 +1,19 @@
 ﻿
 namespace DO;
 
-    public struct Order
-    {
-    }
+
+public struct Order
+{
+    private int ID { get; set; }
+    private string CustomerName { get; set; }
+    private string CustomerEmail { get; set; }
+    private string CustomerAdress { get; set; }
+    private DateTime OrderDate  { get; set; }
+    private DateTime ShipDate { get; set; } 
+    private DateTime DeliveryrDate { get; set; }
+
+
+
+
+}
 
