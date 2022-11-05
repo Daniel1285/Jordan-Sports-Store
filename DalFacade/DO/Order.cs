5820a0ea1b1adec8732478399b6a6989f1931367
נתן שטרן <natstern@g.jct.ac.il>
2022-11-05 23:39:16 +0200

    Add comments

--- DalFacade/DO/Order.cs
+++ DalFacade/DO/Order.cs
@@ -6,13 +6,45 @@ namespace DO;
 /// </summary>
 public struct Order
 {
+    /// <summary>
+    /// Identification number
+    /// </summary>
     private int ID { get; set; }
+    /// <summary>
+    /// The name of the ordering customer
+    /// </summary>
     private string CustomerName { get; set; }
+    /// <summary>
+    /// Customer's email address
+    /// </summary>
     private string CustomerEmail { get; set; }
+    /// <summary>
+    /// Customer's address
+    /// </summary>
     private string CustomerAdress { get; set; }
+    /// <summary>
+    /// Order creation date
+    /// </summary>
     private DateTime OrderDate  { get; set; }
-    private DateTime ShipDate { get; set; } 
+    /// <summary>
+    /// delivery date
+    /// </summary>
+    private DateTime ShipDate { get; set; }
+    /// <summary>
+    /// Date of delivery
+    /// </summary>
     private DateTime DeliveryrDate { get; set; }
+    public override string ToString() => $@"
+        Product ID={ID}
+        Customer Name:{CustomerName}
+        Customer Email:{CustomerEmail}
+        Customer Adress:{CustomerAdress}
+        Order Date:{OrderDate}
+        Ship Date:{ShipDate}
+        DeliveryrDate:{DeliveryrDate}
+        ";
+   
+    
 
 }
 
