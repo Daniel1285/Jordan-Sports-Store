e6fec439f1c6109d780273a8da388f962b7c083d
נתן שטרן <natstern@g.jct.ac.il>
2022-11-20 19:00:13 +0200

    akuna matte

--- DalList/DataSource.cs
+++ DalList/DataSource.cs
@@ -17,9 +17,9 @@ internal static class DataSource
     internal static class Config
     {
         // Size arrays in real time
-        internal static int SizeOfProducts = 0;
-        internal static int SizeOfOrder = 0;
-        internal static int SizeOfOrderItem = 0;
+        //internal static int SizeOfProducts = 0;
+        //internal static int SizeOfOrder = 0;
+       // internal static int SizeOfOrderItem = 0;
 
 
         internal static int MinOrder = 100000;
@@ -39,9 +39,9 @@ internal static class DataSource
 
     private static readonly Random R = new Random(); // Random number generation
 
-    internal static Product[] MyProducts = new Product[50];
-    internal static Order[] MyOrder = new Order[100];
-    internal static OrderItem[] MyOrderItem = new OrderItem[200];
+    internal static List<Product>  MyProducts = new List<Product>();
+    internal static List<Order> MyOrder = new List<Order>();
+    internal static List<OrderItem> MyOrderItem = new List<OrderItem>();
 
 
 
@@ -60,7 +60,7 @@ internal static class DataSource
                                          "socks - Nike Everyday Plus Cushioned", "shirt - Nike Pro Dri-FIT", "shirt - Nike Dri-FIT Legend"};
         Double[] priceOfProduct = { 550, 400, 250, 332.5, 210, 129.99, 49, 63, 122.5, 140 };
         Enums.Category[] categories = { Category.SHOES, Category.SHOES, Category.SHORTS, Category.SHOES, Category.HOODIES, Category.HOODIES, Category.SOCKS, Category.SOCKS, Category.SHIRTS, Category.SHIRTS };
-        int[] AmountInSoke = {0,7,5,4,2,9,5,6,3,10 };
+        int[] AmountInSoke = { 0, 7, 5, 4, 2, 9, 5, 6, 3, 10 };
         for (int i = 0; i < size; i++)
         {
             Product p = new Product
@@ -74,10 +74,10 @@ internal static class DataSource
             };
             if (CheckID(p.ID))
             {
-                Config.SizeOfProducts++;
-                MyProducts[Config.SizeOfProducts++] = p;
+                MyProducts.Add(p);
             }      
         }
+
         
     }
 
@@ -97,10 +97,12 @@ internal static class DataSource
                 OrderID = Config.GetIdForOrder,
                 ProductID = R.Next(100000),
                 Price = R.Next(200, 300),
-                Amount = Math.Min(R.Next(1, 5), MyProducts[Config.SizeOfOrderItem].InStock)
+                Amount = Math.Min(R.Next(1, 5), MyProducts.Find().InStock,
+                // Amount = Math.Min( R.Next(1, 5), MyProducts[Config.SizeOfOrderItem].InStock)
             };
 
             MyProducts[Config.SizeOfOrderItem].InStock -= NewOrderItem.Amount;
+            MyOrderItem.Add(NewOrderItem);
            
         }
         
@@ -141,7 +143,7 @@ internal static class DataSource
             if (i <  10)
                 newOrder.DeliveryrDate = newOrder.ShipDate + new TimeSpan(R.Next(1, 3),0,0,0);
 
-            MyOrder[Config.SizeOfOrder++] = newOrder; 
+            MyOrder.Add(newOrder); 
         }
 
     }
