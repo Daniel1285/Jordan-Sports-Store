5fe6041876027f2ec7ae9200dde753fce268f412
נתן שטרן <natstern@g.jct.ac.il>
2022-11-23 13:23:55 +0200

    Update Bi calss

---
+++ BL/BlImplementation/Bl.cs
@@ -0,0 +1,15 @@
+﻿
+using BlApi;
+using System.Security.Principal;
+
+namespace BlImplementation
+{
+    sealed public class Bl: IBl
+    {
+        public ICart Cart => new Cart();
+        public IOrder Order => new Order();
+        public IProduct Product => new Product();   
+
+
+    }
+}
