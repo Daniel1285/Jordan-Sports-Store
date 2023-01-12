ad33d0bdac7e4d4a6edbd1cef7da7d9949488023
daniel_cohen <daniel1cohen13579@gmail.com>
2023-01-12 18:49:39 +0200

    edid nams of  folder xml

--- DalXml/DalProduct.cs
+++ DalXml/DalProduct.cs
@@ -1,4 +1,5 @@
 ﻿using DalApi;
+using DO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,15 @@ namespace Dal;
 
 internal class DalProduct :IProduct
 {
+    const string s_product = @"Product"; //XML Serializer
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="p"></param>
+    /// <returns></returns>
+    public int Add(Product p)
+    {
+        List<Product?> products = XMLTools.LoadListFromXMLSerializer<Product>(s_product);  
 
+    }
 }
