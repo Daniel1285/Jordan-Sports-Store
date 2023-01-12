334739b86b5803e7ddb4410bb4442bfe099830c5
נתן שטרן <natstern@g.jct.ac.il>
2023-01-12 19:21:14 +0200

    UPDate dalxml order

--- DalXml/DalOrder.cs
+++ DalXml/DalOrder.cs
@@ -1,4 +1,5 @@
 ﻿using DalApi;
+using DO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,83 @@ namespace Dal;
 
 internal class DalOrder : IOrder
 {
+    const string s_Order = @"Order";
+    /// <summary>
+    /// Add a Order to xml file
+    /// </summary>
+    /// <param name="o"></param>
+    /// <returns></returns>
+    public int Add(Order o)
+    {
+        //o.ID = 
+        List<Order?> list = XMLTools.LoadListFromXMLSerializer<Order>(s_Order);
+        list.Add(o);
+        XMLTools.SaveListToXMLSerializer(list, s_Order);
+        return o.ID;
+    }
+    /// <summary>
+    /// Deleteing a Order from the the xml file Order.
+    /// </summary>
+    /// <param name="id"></param>
+    public void Delete(int id)
+    {
+        List<Order?> list = XMLTools.LoadListFromXMLSerializer<Order>(s_Order);
+        if(list.RemoveAll(x => x?.ID == id) == 0)
+        {
+            throw new NotExistException();
+        }
+        XMLTools.SaveListToXMLSerializer(list, s_Order);
+    }
+    /// <summary>
+    /// Updates a Order by overwriting an existing Order in xml file.
+    /// </summary>
+    /// <param name="o"></param>
+    /// <exception cref="Exception"></exception>
+    public void Update(Order o)
+    {
+        Delete(o.ID);
+        Add(o);
+    }
+    /// <summary>
+    /// Returns All Orders in the file.
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerable<Order?> GetAll(Func<Order?, bool>? filter)
+    {
+        IEnumerable<Order?> list = XMLTools.LoadListFromXMLSerializer<Order>(s_Order);
+        if(filter == null)
+        {
+            list = from item in list
+                   select item;
+            return list;
+        }
+        else
+        {
+            list = from item in list
+                   where (filter(item))
+                   select item;
+            return list;
+        }
+    }
+    /// <summary>
+    /// Receives a function for testing (for example, ID resonance) and returns an object according to this
+    /// </summary>
+    /// <param name="filter"></param>
+    /// <returns></returns>
+    /// <exception cref="DO.NotExistException"></exception>
+    public Order GetByCondition(Func<Order?, bool>? filter)
+    {
+        IEnumerable<Order?> list = XMLTools.LoadListFromXMLSerializer<Order>(s_Order);
+        var order = list.FirstOrDefault(filter!);
+        if(order != null)
+        {
+            return (Order)order;
+        }
+        throw new NotExistException("NOT exists!");
+    }
+
+
+
+
 
 }
