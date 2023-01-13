d9b3330012346bfcfca9f59b2c80592902083b18
daniel_cohen <daniel1cohen13579@gmail.com>
2023-01-13 04:26:28 +0200

    update tracking page

--- PL/PagesManager/ProductsPage.xaml.cs
+++ PL/PagesManager/ProductsPage.xaml.cs
@@ -18,88 +18,87 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace PL.PagesManager
+namespace PL.PagesManager;
+
+/// <summary>
+/// Interaction logic for ProductsPage.xaml
+/// </summary>
+public partial class ProductsPage : Page,INotifyPropertyChanged
 {
-    /// <summary>
-    /// Interaction logic for ProductsPage.xaml
-    /// </summary>
-    public partial class ProductsPage : Page,INotifyPropertyChanged
+    private BlApi.IBl? Bl = BlApi.Factory.Get();
+
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private void OnPropertyChanged(string propertyName)
     {
-        private BlApi.IBl? Bl = BlApi.Factory.Get();
-
-
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        private void OnPropertyChanged(string propertyName)
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));   
+    }
+    private ObservableCollection<BO.ProductForList?> _myListProduct;
+    public ObservableCollection<BO.ProductForList?>  myListProduct
+    {
+        get { return _myListProduct; }
+        set
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));   
+            _myListProduct = value;
+            OnPropertyChanged(nameof(myListProduct));
         }
-        private ObservableCollection<BO.ProductForList?> _myListProduct;
-        public ObservableCollection<BO.ProductForList?>  myListProduct
-        {
-            get { return _myListProduct; }
-            set
-            {
-                _myListProduct = value;
-                OnPropertyChanged(nameof(myListProduct));
-            }
-        }
+    }
 
-        private string _ssd;
-        public string ssd 
-        { 
-            get { return _ssd; }
-            set { _ssd = value; OnPropertyChanged(nameof(ssd)); }
-        }
-        public Array Categories { get { return Enum.GetValues(typeof(BO.Enums.Category));}}
-        public BO.Enums.Category Category { get; set; }
-        public ProductsPage()
+    private string _ssd;
+    public string ssd 
+    { 
+        get { return _ssd; }
+        set { _ssd = value; OnPropertyChanged(nameof(ssd)); }
+    }
+    public Array Categories { get { return Enum.GetValues(typeof(BO.Enums.Category));}}
+    public BO.Enums.Category Category { get; set; }
+    public ProductsPage()
+    {
+        myListProduct = new ObservableCollection<BO.ProductForList?>(Bl!.Product.GetProductList());
+        ssd = "";
+        InitializeComponent();          
+    }
+    private void AttributeSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (AttributeSelector.SelectedItem.ToString() == BO.Enums.Category.NONE.ToString())
         {
+            myListProduct =  new ObservableCollection<BO.ProductForList?>(Bl!.Product.GetProductList());
+
+        }
+        else
+            myListProduct = new ObservableCollection<BO.ProductForList?>(Bl!.Product.GetListByCondition(x => x?.Category.ToString() == AttributeSelector.SelectedItem.ToString()));
+        
+    }
+
+    private void doubleClick_Update(object sender, MouseButtonEventArgs e)
+    {
+
+        if (ProductsListView.SelectedItem != null)
+        {
+            int id = ((BO.ProductForList)ProductsListView.SelectedItem).ID;
+            new AddAndUpdate(id).Show();  
+            (Window.GetWindow(this)).Close();
+               
+        }
+        else
+            MessageBox.Show("Please chose only from the products", "EROOR", MessageBoxButton.OK, MessageBoxImage.Error);
+
+    }
+
+    private void DeleteProductManager_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            int id = ((BO.ProductForList)ProductsListView.SelectedItem).ID;
+            Bl?.Product.DeleteProduct(id);
             myListProduct = new ObservableCollection<BO.ProductForList?>(Bl!.Product.GetProductList());
-            ssd = "";
-            InitializeComponent();          
-        }
-        private void AttributeSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (AttributeSelector.SelectedItem.ToString() == BO.Enums.Category.NONE.ToString())
-            {
-                myListProduct =  new ObservableCollection<BO.ProductForList?>(Bl!.Product.GetProductList());
-
-            }
-            else
-                myListProduct = new ObservableCollection<BO.ProductForList?>(Bl!.Product.GetListByCondition(x => x?.Category.ToString() == AttributeSelector.SelectedItem.ToString()));
+            ssd = "Product deleted successfully";  
             
         }
-
-        private void doubleClick_Update(object sender, MouseButtonEventArgs e)
+        catch (BO.CanNotDeleteProductException)
         {
-
-            if (ProductsListView.SelectedItem != null)
-            {
-                int id = ((BO.ProductForList)ProductsListView.SelectedItem).ID;
-                new AddAndUpdate(id).Show();  
-                (Window.GetWindow(this)).Close();
-                   
-            }
-            else
-                MessageBox.Show("Please chose only from the products", "EROOR", MessageBoxButton.OK, MessageBoxImage.Error);
-
-        }
-
-        private void DeleteProductManager_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                int id = ((BO.ProductForList)ProductsListView.SelectedItem).ID;
-                Bl?.Product.DeleteProduct(id);
-                myListProduct = new ObservableCollection<BO.ProductForList?>(Bl!.Product.GetProductList());
-                ssd = "Product deleted successfully";  
-                
-            }
-            catch (BO.CanNotDeleteProductException)
-            {
-                ssd = "Can't delete a product because it's in the middle of an order.";
-            }
+            ssd = "Can't delete a product because it's in the middle of an order.";
         }
     }
 }
--- PL/PlCart/TruckingOrder/MainTrucking.xaml.cs
+++ PL/PlCart/TruckingOrder/MainTrucking.xaml.cs
@@ -2,6 +2,7 @@
 using PL.PlProduct;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Reflection.Emit;
 using System.Text;
@@ -16,55 +17,71 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace PL.PlCart.TruckingOrder
+namespace PL.PlCart.TruckingOrder;
+
+/// <summary>
+/// Interaction logic for MainTrucking.xaml
+/// </summary>
+public partial class MainTrucking : Page, INotifyPropertyChanged
 {
-    /// <summary>
-    /// Interaction logic for MainTrucking.xaml
-    /// </summary>
-    public partial class MainTrucking : Page
+    private BlApi.IBl? Bl = BlApi.Factory.Get();
+    public BO.Order order = new BO.Order();
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    private void OnPropertyChanged(string propertyName)
     {
-        private BlApi.IBl? Bl = BlApi.Factory.Get();
-        public BO.Order order = new BO.Order();
-        public MainTrucking()
-        {
-            InitializeComponent();
-        }
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 
-        private void Trucking_Click(object sender, RoutedEventArgs e)
+    private string _strResult;
+    public string strResult
+    {
+        get { return _strResult; }
+        set 
         {
-            int IDTrucking = int.Parse(truck.Text);
-            try
-            {
-                BO.Order? a = new BO.Order();
-                a = Bl?.Order.GetOrder(IDTrucking);
-                ResultTrucking.Text = "";
-                ResultTrucking.Text = (a?.ID + "\n" + a?.OrderDate + "\n" + a?.Status).ToString();
-                order = Bl?.Order.GetOrder(IDTrucking)!;
-                truckingOrder.Visibility = Visibility.Hidden;
-                ViewOrderItemButton.Visibility = Visibility.Visible;
-                order = Bl.Order.GetOrder(IDTrucking);
-            }
-            catch (BO.NotExistException)
-            {
-                ResultTrucking.Text = "ID not found !" + "\n   please try again.";
-            }
+            _strResult = value;
+            OnPropertyChanged(nameof(strResult));
         }
+    }
 
-        private void BackToLastWindowButton_Click(object sender, RoutedEventArgs e)
-        {
-            new MainWindow().Show();
-            Window.GetWindow(this).Close();  
-        }
+    public MainTrucking()
+    {
+        strResult = "";
+        InitializeComponent();
+    }
 
-        private void ViewOrderItem_Click(object sender, RoutedEventArgs e)
+    private void Trucking_Click(object sender, RoutedEventArgs e)
+    {
+        int IDTrucking = int.Parse(truck.Text);
+        try
         {
-            truckingFram.Content = new OrderItemView(order.ID);
-            ViewOrderItemButton.Visibility = Visibility.Hidden;
+            BO.Order? a = new BO.Order();
+            a = Bl?.Order.GetOrder(IDTrucking);
+            strResult = $" {a?.ID} \n {a?.OrderDate} \n {a?.Status}";
+            truckingOrder.Visibility = Visibility.Hidden;
+            ViewOrderItemButton.Visibility = Visibility.Visible;
+            order = Bl?.Order.GetOrder(IDTrucking);
         }
+        catch (BO.NotExistException)
+        {
+            strResult = "ID not found ! \n   please try again.";
+        }
+    }
 
-        private void myFrame_ContentRendered(object sender, EventArgs e)
-        {
-            truckingFram.NavigationUIVisibility = System.Windows.Navigation.NavigationUIVisibility.Hidden;
-        }
+    private void BackToLastWindowButton_Click(object sender, RoutedEventArgs e)
+    {
+        new MainWindow().Show();
+        Window.GetWindow(this).Close();  
+    }
+
+    private void ViewOrderItem_Click(object sender, RoutedEventArgs e)
+    {
+        truckingFram.Content = new OrderItemView(order.ID);
+        ViewOrderItemButton.Visibility = Visibility.Hidden;
+    }
+
+    private void myFrame_ContentRendered(object sender, EventArgs e)
+    {
+        truckingFram.NavigationUIVisibility = System.Windows.Navigation.NavigationUIVisibility.Hidden;
     }
 }
